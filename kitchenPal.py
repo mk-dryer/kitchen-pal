4172bfce0e1077110d2c14649012cf36b66278a1
Mary Kathleen <45740177+mk-dryer@users.noreply.github.com>
2024-10-09 06:42:46 -0400

    commit .py and .db files

---
+++ kitchenPal.py
@@ -0,0 +1,88 @@
+
+# import libs and functions 
+
+import random
+import sqlite3
+
+# connect to database 
+
+conn = sqlite3.connect(r"C:\Users\MK\Desktop\python projects\kitchen-pal\recipes.db")
+cursor = conn.cursor()
+
+# create a table to store recipes if not already exists 
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS recipes (
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         name TEXT,
+         ingredients TEXT, 
+         link TEXT
+     )
+ """)
+conn.commit()
+
+
+# initialize an empty list to store selected & declined recipes
+selected_recipes = []
+declined_recipes = []
+
+
+# function to propose recipes 
+def propose_recipe():
+    global done # declare var done 
+    cursor.execute("SELECT name, ingredients FROM recipes") # select name & ingredients cols from recipe entries 
+    rows = cursor.fetchall() # fetch all rows 
+    
+    while not done: # while done = False: 
+        known_recipes = [{'name': row[0], 'ingredients': row[1]} for row in rows] # process all recipe name & ingredients as dict 
+        available_recipes = [recipe for recipe in known_recipes if recipe not in selected_recipes and recipe not in declined_recipes] # available recipes = all known recipes not yet selected or declined 
+        if not available_recipes: # if no recipes available: 
+            print("You've already selected all available recipes.")
+            done = True
+            break
+        
+        recipe = random.choice(available_recipes) # randomly select from list of available recipes 
+        print(f"Do you want to make {recipe['name']}? (y/n)") # propose recipe to user 
+        choice = input().strip().lower() # process input 
+        if choice == 'y':
+            selected_recipes.append(recipe) # append to selected list 
+            print(f"{recipe['name']} added to your list.") 
+        else: 
+            declined_recipes.append(recipe) # append to declined 
+            print("No problem.") # and proceed to next choice 
+        another_choice = input("Do you want to choose another recipe? (y/n) ").strip().lower()
+        if another_choice != 'y':
+            gen_list = input("Are you ready to generate your shopping list? (y/n) ").strip().lower()
+            if gen_list == "y":
+                done = True
+                break
+
+
+# function to generate a shopping list
+def generate_shopping_list(): 
+    shopping_list = [] # intialize empty shopping list 
+    for recipe in selected_recipes: # for each recipe 
+        ingredients = recipe['ingredients'] # access ingredients 
+        for ingredient in ingredients.split(","): # split by comma  
+            shopping_list.append(ingredient.strip()) # and append to shopping list 
+    shopping_list = sorted(list(set(shopping_list))) # remove duplicates and sort the shopping list
+    return shopping_list
+
+# main program
+print("Welcome to the Dinner Planner!")
+done = False # declare done as False to trigger loop 
+propose_recipe() # invoke propose recipe 
+if done: # once done = True: 
+    print("Great! You've selected the following recipes:") # review selected recipes 
+    for recipe in selected_recipes:
+        print(recipe['name'])
+    shopping_list = generate_shopping_list() # invoke generate list
+    print("\nHere's your shopping list:")
+    for item in shopping_list:
+        print(item)
+else:
+    print("You haven't selected any recipes. Have a great day!")
+
+conn.close()
+
+## INSERT INTO recipes (name, ingredients, link) VALUES ("Lemon Garlic Butter Chicken and Brussels Sprouts", "brussels sprouts, olive oil, salt, pepper, paprika, chicken tenderloins, Italian seasoning, red pepper flakes, lemon juice, butter", "https://juliasalbum.com/lemon-garlic-butter-chicken/")y
